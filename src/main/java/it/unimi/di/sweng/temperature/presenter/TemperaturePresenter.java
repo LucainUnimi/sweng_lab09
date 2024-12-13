ab18567fe3c0f67a5ce359abbea238e696d3131c
Luca Dal Mas <luca.dalmas@studenti.unimi.it>
2024-12-13 15:57:40 +0100

    VERDE: Presenter use Strategy

--- src/main/java/it/unimi/di/sweng/temperature/presenter/TemperaturePresenter.java
+++ src/main/java/it/unimi/di/sweng/temperature/presenter/TemperaturePresenter.java
@@ -12,6 +12,6 @@ public class TemperaturePresenter implements Presenter {
 
     @Override
     public void action(@NotNull String text) {
-        //strategy.convertToCelsius(Double.parseDouble(text));
+        strategy.convertToCelsius(Double.parseDouble(text));
     }
 }
