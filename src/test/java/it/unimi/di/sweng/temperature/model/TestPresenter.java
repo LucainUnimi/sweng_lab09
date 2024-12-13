4db64cd9299bf7dbc63f3c11a790bee5937944a0
Luca Dal Mas <luca.dalmas@studenti.unimi.it>
2024-12-13 16:16:58 +0100

    ROSSO: Presenter Observr

--- src/test/java/it/unimi/di/sweng/temperature/model/TestPresenter.java
+++ src/test/java/it/unimi/di/sweng/temperature/model/TestPresenter.java
@@ -1,8 +1,12 @@
 package it.unimi.di.sweng.temperature.model;
 
+import it.unimi.di.sweng.temperature.Observable;
+import it.unimi.di.sweng.temperature.Observer;
+import it.unimi.di.sweng.temperature.presenter.CelsiusStrategy;
 import it.unimi.di.sweng.temperature.presenter.Presenter;
 import it.unimi.di.sweng.temperature.presenter.ScaleStrategy;
 import it.unimi.di.sweng.temperature.presenter.TemperaturePresenter;
+import it.unimi.di.sweng.temperature.view.View;
 import javafx.scene.transform.Scale;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
@@ -16,10 +20,22 @@ public class TestPresenter {
     @Test
     void testStrategy() {
         ScaleStrategy DOC = mock();
-        Presenter SUT = new TemperaturePresenter(DOC);
+        TemperaturePresenter SUT = new TemperaturePresenter(mock(), mock());
+        SUT.setStategy(DOC);
         SUT.action("10.45");
 
         verify(DOC).convertToCelsius(10.45);
-
     }
+
+    @Test
+    void testUpdate() {
+        TemperatureModel mockedModel = mock();
+        View mockedView = mock();
+        TemperaturePresenter SUT = new TemperaturePresenter(mockedView, mockedModel);
+        Double stato = 42.42;
+
+        SUT.update(mockedModel, stato);
+        verify(mockedView).setValue(stato.toString());
+    }
+
 }
