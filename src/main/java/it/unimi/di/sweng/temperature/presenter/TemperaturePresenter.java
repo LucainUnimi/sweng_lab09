23d9dd73d4bdd80274677ad3ecb0a6d102493270
Luca Dal Mas <luca.dalmas@studenti.unimi.it>
2024-12-13 15:57:24 +0100

    ROSSO: Presenter use Strategy

--- src/main/java/it/unimi/di/sweng/temperature/presenter/TemperaturePresenter.java
+++ src/main/java/it/unimi/di/sweng/temperature/presenter/TemperaturePresenter.java
@@ -1,4 +1,17 @@
 package it.unimi.di.sweng.temperature.presenter;
 
-public class TemperaturePresenter {
+import it.unimi.di.sweng.temperature.Observable;
+import org.jetbrains.annotations.NotNull;
+
+public class TemperaturePresenter implements Presenter {
+    private final ScaleStrategy strategy;
+
+    public TemperaturePresenter(ScaleStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    @Override
+    public void action(@NotNull String text) {
+        //strategy.convertToCelsius(Double.parseDouble(text));
+    }
 }
--- src/test/java/it/unimi/di/sweng/temperature/model/TestPresenter.java
+++ src/test/java/it/unimi/di/sweng/temperature/model/TestPresenter.java
@@ -1,8 +1,25 @@
 package it.unimi.di.sweng.temperature.model;
 
+import it.unimi.di.sweng.temperature.presenter.Presenter;
+import it.unimi.di.sweng.temperature.presenter.ScaleStrategy;
+import it.unimi.di.sweng.temperature.presenter.TemperaturePresenter;
+import javafx.scene.transform.Scale;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 @Timeout(2)
 public class TestPresenter {
+
+    @Test
+    void testStrategy() {
+        ScaleStrategy DOC = mock();
+        Presenter SUT = new TemperaturePresenter(DOC);
+        SUT.action("10.45");
+
+        verify(DOC).convertToCelsius(10.45);
+
+    }
 }
